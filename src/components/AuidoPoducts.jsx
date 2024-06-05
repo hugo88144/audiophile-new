38faf4c1a26a4de97a51b3c4d38c7382cdb8f326
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-06-06 00:11:56 +0100

    ok

--- src/components/AuidoPoducts.jsx
+++ src/components/AuidoPoducts.jsx
@@ -1,5 +1,45 @@
-function AuidoPoducts() {
-  return <div>hello</div>;
+import headphonesImg from "../assets/shared/desktop/image-category-thumbnail-headphones.png";
+import speakersImg from "../assets/shared/desktop/image-category-thumbnail-speakers.png";
+import earphonesImg from "../assets/shared/desktop/image-category-thumbnail-earphones.png";
+import arrow from "../assets/shared/desktop/icon-arrow-right.svg";
+
+function AudioProducts() {
+  return (
+    <div className="audioBox">
+      <div className="audioBox__link">
+        <img src={headphonesImg} alt="Headphones" className="audioBox__img" />
+        <div className="audioBox__title">Headphones</div>
+        <button className="audioBox__btn">
+          Shop{" "}
+          <span className="audioBox__arrow">
+            <img src={arrow} alt="arrow" />
+          </span>
+        </button>
+      </div>
+
+      <div className="audioBox__link">
+        <img src={speakersImg} alt="Speakers" className="audioBox__img" />
+        <div className="audioBox__title">Speakers</div>
+        <button className="audioBox__btn">
+          Shop{" "}
+          <span className="audioBox__arrow">
+            <img src={arrow} alt="arrow" />
+          </span>
+        </button>
+      </div>
+
+      <div className="audioBox__link">
+        <img src={earphonesImg} alt="Earphones" className="audioBox__img" />
+        <div className="audioBox__title">Earphones</div>
+        <button className="audioBox__btn">
+          Shop{" "}
+          <span className="audioBox__arrow">
+            <img src={arrow} alt="arrow" />
+          </span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default AuidoPoducts;
+export default AudioProducts;
