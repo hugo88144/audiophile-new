6cbb085b99a356c54fc5235d07613eda0febe143
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-06-05 20:30:36 +0100

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import "./sass/main.scss";
 
-import NavBar from "./components/nav";
+import NavBar from "./components/Nav";
+import HeaderContent from "./components/HeaderContent";
 
 function App() {
   return (
     <div className="container">
       <NavBar />
-      <div className="content">
-        <div className="content__new">New product </div>
-        <div className="content__title">XX99 Mark II Headphones </div>
-        <p className="content__description">
-          Experience natural, lifelike audio and exceptional build quality made
-          for the passionate music enthusiast.
-        </p>
-        <button className="content__btn-see"> See product </button>
-      </div>
+      <HeaderContent />
     </div>
   );
 }
