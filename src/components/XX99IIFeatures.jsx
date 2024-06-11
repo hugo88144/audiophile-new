a57c0ce8928f3857bbfe644bf1c4faf87fa4275b
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-06-11 22:41:39 +0100

    okay

---
+++ src/components/XX99IIFeatures.jsx
@@ -0,0 +1,59 @@
+function XX99Features() {
+  return (
+    <div className="Features">
+      <div className="Features__Description">
+        <div className="Features__Title">FEATURES</div>
+        <div className="Features__DescriptionText">
+          Featuring a genuine leather head strap and premium earcups, these
+          headphones deliver superior comfort for those who like to enjoy
+          endless listening. It includes intuitive controls designed for any
+          situation. Whether you’re taking a business call or just in your own
+          personal space, the auto on/off and pause features ensure that you’ll
+          never miss a beat. The advanced Active Noise Cancellation with
+          built-in equalizer allow you to experience your audio world on your
+          terms. It lets you enjoy your audio in peace, but quickly interact
+          with your surroundings when you need to. Combined with Bluetooth 5. 0
+          compliant connectivity and 17 hour battery life, the XX99 Mark II
+          headphones gives you superior sound, cutting-edge technology, and a
+          modern design aesthetic.
+        </div>
+      </div>
+      ;
+      <div className="Features__includes">
+        <div className="Features__includes-title">IN THE BOX</div>
+
+        <div className="Features__includes-quantityBox">
+          <div className="Features__includes-quantity">
+            <span>1x</span> Headphone Unit
+          </div>
+        </div>
+
+        <div className="Features__includes-quantityBox">
+          <div className="Features__includes-quantity">
+            <span>2x</span>Replacement earcups
+          </div>
+        </div>
+
+        <div className="Features__includes-quantityBox">
+          <div className="Features__includes-quantity">
+            <span>1x</span>User manual
+          </div>
+        </div>
+
+        <div className="Features__includes-quantityBox">
+          <div className="Features__includes-quantity">
+            <span>1x</span>3.5mm 5m audio cable
+          </div>
+        </div>
+
+        <div className="Features__includes-quantityBox">
+          <div className="Features__includes-quantity">
+            <span>1x</span>Travel bag
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default XX99Features;
--- src/pages/XX99II.jsx
+++ src/pages/XX99II.jsx
@@ -7,11 +7,13 @@ import XX99IIImgs from "../components/XX99IIImgs";
 import XX99MARKIcard from "../components/XX99MARKIcard.jsx";
 import XX59card from "../components/XX59card.jsx";
 import ZX9card from "../components/ZX9card.jsx";
+import XX99IIFeatures from "../components/XX99IIFeatures.jsx";
 
 function XX99II() {
   return (
     <div className="box">
       <NavBar />
+      <XX99IIFeatures />
       <XX99IIImgs />
       <AudioProductsV2>
         <XX99MARKIcard />
