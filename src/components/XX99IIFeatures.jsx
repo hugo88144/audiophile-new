974ae645fca3894af16c8b6ce89097c667687b44
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-06-13 22:28:40 +0100

    features

--- src/components/XX99IIFeatures.jsx
+++ src/components/XX99IIFeatures.jsx
@@ -9,13 +9,16 @@ function XX99Features() {
           endless listening. It includes intuitive controls designed for any
           situation. Whether you’re taking a business call or just in your own
           personal space, the auto on/off and pause features ensure that you’ll
-          never miss a beat. The advanced Active Noise Cancellation with
-          built-in equalizer allow you to experience your audio world on your
-          terms. It lets you enjoy your audio in peace, but quickly interact
-          with your surroundings when you need to. Combined with Bluetooth 5. 0
-          compliant connectivity and 17 hour battery life, the XX99 Mark II
-          headphones gives you superior sound, cutting-edge technology, and a
-          modern design aesthetic.
+          never miss a beat.
+        </div>
+
+        <div className="features__DescriptionText">
+          The advanced Active Noise Cancellation with built-in equalizer allow
+          you to experience your audio world on your terms. It lets you enjoy
+          your audio in peace, but quickly interact with your surroundings when
+          you need to. Combined with Bluetooth 5. 0 compliant connectivity and
+          17 hour battery life, the XX99 Mark II headphones gives you superior
+          sound, cutting-edge technology, and a modern design aesthetic.
         </div>
       </div>
       ;
