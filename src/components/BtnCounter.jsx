493f7d9b563901e6c916a7f1501b5cc7e795952c
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-06-14 01:49:56 +0100

    okay

--- src/components/BtnCounter.jsx
+++ src/components/BtnCounter.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 function BtnCounter() {
+  const [quantity, setQuantity] = useState(0);
+  function Handleincrement() {
+    setQuantity(quantity + 1);
+  }
+  function Handledecrement() {
+    setQuantity(quantity - 1);
+  }
+
   return (
     <div className="btnCounter">
-      <button className="btnCounter__minus">-</button>
-      <span className="btnCounter__quantity">0</span>
-      <button className="btnCounter__plus">+</button>
+      <button className="btnCounter__minus" onClick={Handledecrement}>
+        -
+      </button>
+      <span className="btnCounter__quantity">{quantity}</span>
+      <button className="btnCounter__plus" onClick={Handleincrement}>
+        +
+      </button>
     </div>
   );
 }
